e7bef67061c0af396320323563da2d1e4274d5c3
rsaee <ridasaeed@cmail.carleton.ca>
2022-11-29 17:48:42 -0500

    added ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QPixmap>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap pix("/home/student/Desktop/COMP3004-Project/images/device.PNG");
+    int w = ui->rmb->width();
+    int h = ui->rmb->height();
+    ui->rmb->setPixmap(pix.scaled(w, h, Qt::KeepAspectRatio));
+//    ui->userMenu.setVisible(false);
+//    ui->mainMenu.setVisible(false);
+//    ui->groupMenu.setVisible(false);
 }
 
 MainWindow::~MainWindow()
